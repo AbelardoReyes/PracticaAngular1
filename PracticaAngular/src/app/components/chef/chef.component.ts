840b6e1c9ea6b53d550b169c84a9592d5849dd73
Abelardo <abelardoreyes256@gmail.com>
2023-02-20 20:02:27 -0600

    Revert "Ya obtuve el tokesn"
    This reverts commit e9f889818054198d7cbdf33c59fa8926823498b9.

--- PracticaAngular/src/app/components/chef/chef.component.ts
+++ PracticaAngular/src/app/components/chef/chef.component.ts
@@ -1,34 +1,27 @@
-import { Component, inject } from '@angular/core';
-import { Chef } from 'src/app/models/chef.model';
+import { Component } from '@angular/core';
+import { Chef } from 'src/app/Models/chef.model';
 import { ChefService } from 'src/app/services/chef.service';
 import { OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { TokenInterceptor } from 'src/app/Interceptors/token.interceptor';
-import { HttpInterceptor } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+
 @Component({
   selector: 'app-chef',
   templateUrl: './chef.component.html',
   styleUrls: ['./chef.component.css']
 })
-
 export class ChefComponent implements OnInit, OnDestroy {
   chefs: Chef[] = [];
-  suscription?: Subscription;
-  private token: string = '';
+  suscription?:Subscription;
 
-  constructor(private chefService: ChefService,
-    private httpInterceptor: HttpInterceptor) { }
-
+  constructor(private chefService: ChefService) { }
   ngOnInit(): void {
     this.getChefs();
     this.suscription = this.chefService.get_refresh$().subscribe(() => {
       this.getChefs();
-      this.token = this.httpInterceptor.();
     }
     );
   }
-  ngOnDestroy(): void {
+  ngOnDestroy():void {
     this.suscription?.unsubscribe();
     console.log('Se destruyó el componente');
   }
